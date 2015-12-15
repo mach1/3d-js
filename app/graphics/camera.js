380a866ef07e2f604fd627adf7f7e6dfde18830c
Jaan Lambert <jaan.lambert@gmail.com>
2015-12-15 22:52:00 +0200

    Refactor camera

--- app/graphics/camera.js
+++ app/graphics/camera.js
@@ -8,26 +8,34 @@ export default class Camera {
     this.matrix = this.getMatrix();
   }
 
+  getTranslation(eyeV) {
+    return new Matrix4([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      -eyeV.coordinates[0], -eyeV.coordinates[1], -eyeV.coordinates[2], 1
+    ]);
+  }
+
+  getOrientation(xAxis, yAxis, zAxis) {
+    return new Matrix4([
+      xAxis.get(0), yAxis.get(0), zAxis.get(0), 0,
+      xAxis.get(1), yAxis.get(1), zAxis.get(1), 0,
+      xAxis.get(2), yAxis.get(2), zAxis.get(2), 0,
+      0, 0, 0, 1
+    ]);
+
+  }
+
   getMatrix() {
     this.zAxis = this.eyeV.subtractV(this.targetV);
     this.zAxis = this.zAxis.normalize();
     this.xAxis = this.upV.multiplyV(this.zAxis);
     this.xAxis = this.xAxis.normalize();
     this.yAxis = this.zAxis.multiplyV(this.xAxis);
-    var orientation = new Matrix4([
-      this.xAxis.coordinates[0], this.yAxis.coordinates[0], this.zAxis.coordinates[0], 0,
-      this.xAxis.coordinates[1], this.yAxis.coordinates[1], this.zAxis.coordinates[1], 0,
-      this.xAxis.coordinates[2], this.yAxis.coordinates[2], this.zAxis.coordinates[2], 0,
-      0, 0, 0, 1
-    ]);
 
-    var translation = new Matrix4([
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      -this.eyeV.coordinates[0], -this.eyeV.coordinates[1], -this.eyeV.coordinates[2], 1
-    ]);
-
+    var orientation = this.getOrientation(this.xAxis, this.yAxis, this.zAxis);
+    var translation = this.getTranslation(this.eyeV);
     return orientation.multiplyMM(translation);
 
 
