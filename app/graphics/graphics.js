e60041d0c7bc1b4d79d00af95ed782d94fae6766
Jaan Lambert <jaan.lambert@gmail.com>
2015-12-16 18:31:43 +0200

    Refactor graphics

--- app/graphics/graphics.js
+++ app/graphics/graphics.js
@@ -1,15 +1,15 @@
 import Camera from './camera.js';
 import Vertex4 from '../math/vertex4.js';
 import MatrixHelper from '../math/matrix-helper.js';
-import Cube from '../models/cube.js';
 
 export default class Graphics {
- 
+
   constructor(width, height, canvasId = 'canvas') {
     this.width = width;
     this.height = height;
     this.canvasId = canvasId;
-  } 
+    this.models = [];
+  }
 
   init() {
     this.canvas = document.getElementById(this.canvasId);
@@ -18,18 +18,23 @@ export default class Graphics {
     this.canvas.width = this.width;
     this.perspectiveM = new MatrixHelper().getPerspectiveM(90, this.width / this.height, 1, 10);
     this.camera = new Camera(new Vertex4(5, 3, 3, 1), new Vertex4(0, 0, 0, 1), new Vertex4(0, 1, 0, 1));
-    this.cube = new Cube(new Vertex4(0, 0, 0, 1), 0.5);
 
     requestAnimationFrame(this.render.bind(this));
   }
 
   render(timeStamp) {
     this.clear();
-    this.camera.eyeV = new Vertex4(Math.tan(timeStamp / 2000), -1, Math.tan(timeStamp / 1000), 1);
-    this.cube.render(this);
+    this.camera.eyeV = new Vertex4(Math.sin(timeStamp / 1000), -1, Math.cos(timeStamp / 1000), 1);
+    this.models.forEach(model => {
+      model.render(this);
+    });
     setTimeout(() => {
       requestAnimationFrame(this.render.bind(this));
     }, 1000 / 60);
+  }
+
+  addModel(model) {
+    this.models.push(model);
   }
 
   clear() {
@@ -46,13 +51,12 @@ export default class Graphics {
   toScreenCoordinates(vertex4) {
     var halfWidth = this.width / 2;
     var halfHeight = this.height / 2;
-    var coordinates = vertex4.asArray();
     return new Vertex4(
-      coordinates[0] * halfWidth + halfWidth,
+      vertex4.get(0) * halfWidth + halfWidth,
       // Why is that?
-      coordinates[1] * halfHeight,
-      coordinates[2],
-      coordinates[3]
+      vertex4.get(1) * halfHeight,
+      vertex4.get(2),
+      vertex4.get(3)
     );
   }
 
--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 import Graphics from './graphics/graphics.js';
 import Vertex4 from './math/vertex4.js';
 import Camera from './graphics/camera.js';
+import Cube from './models/cube.js';
 
 var camera = new Camera(new Vertex4(0, 5, 10, 1), new Vertex4(0, 0, -1, 1), new Vertex4(0, 1, 0, 1));
 var graphics = new Graphics(window.innerWidth, window.innerHeight);
 graphics.init();
+
+var cube = new Cube(new Vertex4(0, 0, 0, 1), 0.5);
+graphics.addModel(cube);
